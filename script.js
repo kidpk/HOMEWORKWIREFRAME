957144a987445a8fc1207d70cd4d1bf584255585
Josh Joyce <59264835+kidpk@users.noreply.github.com>
2020-01-28 14:34:51 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -7,47 +7,53 @@ function generatePassword() {
   var nums = "0123456789";
   var syms = "!@#$%^&*=-_";
   var randTypes = ["char"]; // ['char', 'sym', 'num', 'upper']
-
-  var length = prompt("Enter the number of characters in your password?");
-
-  var yesnum = confirm("Do you want to have numbers in your password?");
-
-  var yesSym = confirm("Do you want to have symbols in your password?");
-
-  var yesupper = confirm("Do you want your password to contain uppercase?");
-
-  var charArr = chars.split("");
-
-  var numArr = nums.split("");
-
-  var symsArr = syms.split("");
-
-  var upperArr = upperChars.split("");
-
-  if (yesnum === true) {
-    randTypes.push("nums");
-  }
-
-  if (yesSym === true) {
-    randTypes.push("syms");
-  }
-  if (yesupper === true) {
-    randTypes.push("upperChars");
-  }
-
   var pwd = "";
 
-  for (var i = 0; i < length; i++) {
-    var typeInd = Math.floor(Math.random() * randTypes.length);
-    var type = randTypes[typeInd];
-    if (type === "syms") {
-      pwd += symsArr[i];
-    } else if (type === "nums") {
-      pwd += numArr[i];
-    } else if (type === "upperChars") {
-      pwd += upperArr[i];
-    } else {
-      pwd += charArr[Math.floor(Math.random() * charArr.length)];
+  var length = prompt("Enter the number of characters in your password, NOTHING MORE THAN 128 OR LESS THAN 8!");
+  if (length >= 8 && length <= 128) {
+
+    var yesnum = confirm("Do you want to have numbers in your password?");
+  
+    var yesSym = confirm("Do you want to have symbols in your password?");
+  
+    var yesupper = confirm("Do you want your password to contain uppercase?");
+    
+    
+    
+    var charArr = chars.split("");
+    
+    var numArr = nums.split("");
+    
+    var symsArr = syms.split("");
+    
+    var upperArr = upperChars.split("");
+    
+    if (yesnum === true) {
+      randTypes.push("nums");
+    }
+    
+    if (yesSym === true) {
+      randTypes.push("syms");
+    }
+    if (yesupper === true) {
+      randTypes.push("upperChars");
+    }
+    
+    
+    
+    
+    for (var i = 0; i < length; i++) {
+      var typeInd = Math.floor(Math.random() * randTypes.length);
+      var type = randTypes[typeInd];
+      if (type === "syms") {
+        pwd += symsArr[i];
+      } else if (type === "nums") {
+        pwd += numArr[i];
+      } else if (type === "upperChars") {
+        pwd += upperArr[i];
+      } else {
+        pwd += charArr[Math.floor(Math.random() * charArr.length)];
+      }
     }
   }
   return pwd;
